6d9e423bf543f339c27ca913d3e0770d793800ad
Eduardo Alejandro Pool Aké <eduardo.alejandro.pool.ake@gmail.com>
2015-11-20 15:17:24 -0700

    ListView replaced by RecyclerView.

---
+++ app/src/main/java/com/nearsoft/nearbooks/adapters/realm/BookRecyclerViewAdapter.java
@@ -0,0 +1,48 @@
+package com.nearsoft.nearbooks.adapters.realm;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.nearsoft.nearbooks.R;
+import com.nearsoft.nearbooks.databinding.BookItemBinding;
+import com.nearsoft.nearbooks.models.realm.Book;
+import com.nearsoft.nearbooks.models.viewmodels.BookViewModel;
+
+import io.realm.RealmResults;
+
+/**
+ * Book recycler view adapter.
+ * Created by epool on 11/20/15.
+ */
+public class BookRecyclerViewAdapter extends RealmRecyclerViewAdapter<Book> {
+
+    public BookRecyclerViewAdapter(Context context, RealmResults<Book> realmResults, boolean automaticUpdate) {
+        super(context, realmResults, automaticUpdate);
+    }
+
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        BookItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.book_item, parent, false);
+        return new BookViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Book book = getItem(position);
+        BookItemBinding binding = ((BookViewHolder) holder).binding;
+        binding.setBook(new BookViewModel(book));
+    }
+
+    public static class BookViewHolder extends RecyclerView.ViewHolder {
+        private BookItemBinding binding;
+
+        public BookViewHolder(BookItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/nearsoft/nearbooks/view/helpers/RecyclerItemClickListener.java
@@ -0,0 +1,41 @@
+package com.nearsoft.nearbooks.view.helpers;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Recycler item click listener.
+ * Created by epool on 11/20/15.
+ */
+public class RecyclerItemClickListener extends RecyclerView.SimpleOnItemTouchListener {
+    private OnItemClickListener mListener;
+    private GestureDetector gestureDetector;
+
+    public RecyclerItemClickListener(Context context, OnItemClickListener listener) {
+        mListener = listener;
+        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
+        View childView = view.findChildViewUnder(e.getX(), e.getY());
+        if (childView != null && mListener != null && gestureDetector.onTouchEvent(e)) {
+            mListener.onItemClick(childView, view.getChildAdapterPosition(childView));
+            return true;
+        }
+        return false;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+}
