a1fa86975da019a77442731894ebeafb1d873244
Eduardo Pool <eduardo.alejandro.pool.ake@gmail.com>
2016-04-29 00:07:48 -0700

    Migrate common and configuration classes from Java to Kotlin.

---
+++ app/src/main/java/com/nearsoft/nearbooks/common/Constants.kt
@@ -0,0 +1,14 @@
+package com.nearsoft.nearbooks.common
+
+/**
+ * Constants.
+ * Created by epool on 1/19/16.
+ */
+interface Constants {
+
+    companion object {
+        const val NEARBOOKS_MANAGER_PASSWORD = "admin"
+        const val REGISTRATION_COMPLETE = "REGISTRATION_COMPLETE"
+    }
+
+}
