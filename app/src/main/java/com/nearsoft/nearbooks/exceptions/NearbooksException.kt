f48522f98c5302474e5c8b5eeba288f7769c09dd
Eduardo Pool <eduardo.alejandro.pool.ake@gmail.com>
2016-05-02 18:18:38 -0700

    Migrate exceptions from Java to Kotlin.

--- app/src/main/java/com/nearsoft/nearbooks/exceptions/NearbooksException.kt
+++ app/src/main/java/com/nearsoft/nearbooks/exceptions/NearbooksException.kt
@@ -7,19 +7,11 @@ import android.support.annotation.StringRes
  * Base exception class.
  * Created by epool on 1/20/16.
  */
-open class NearbooksException
-/**
- * Constructs a new `NearbooksException` with the current stack trace and the
- * specified detail message.
-
- * @param detailMessage the detail message for this exception.
- * *
- * @param resourceId    String id of the message to display.
- * *
- * @param formatArgs    arguments for the message to display.
- */
-(detailMessage: String, @StringRes private val mResourceId: Int,
- vararg formatArgs: Any) : Exception(detailMessage) {
+open class NearbooksException(
+        detailMessage: String,
+        @StringRes private val mResourceId: Int,
+        vararg formatArgs: Any
+) : Exception(detailMessage) {
 
     private val mFormatArgs: Array<Any>
 
--- app/src/main/java/com/nearsoft/nearbooks/exceptions/SignInException.kt
+++ app/src/main/java/com/nearsoft/nearbooks/exceptions/SignInException.kt
@@ -6,15 +6,8 @@ import android.support.annotation.StringRes
  * Exception for sign in errors.
  * Created by epool on 1/4/16.
  */
-class SignInException
-/**
- * Constructs a new `NearbooksException` with the current stack trace and the
- * specified detail message.
-
- * @param detailMessage the detail message for this exception.
- * *
- * @param resourceId    String id of the message to display.
- * *
- * @param formatArgs    arguments for the message to display.
- */
-(detailMessage: String, @StringRes resourceId: Int, vararg formatArgs: Any) : NearbooksException(detailMessage, resourceId, formatArgs)
+class SignInException(
+        detailMessage: String,
+        @StringRes resourceId: Int,
+        vararg formatArgs: Any
+) : NearbooksException(detailMessage, resourceId, formatArgs)
