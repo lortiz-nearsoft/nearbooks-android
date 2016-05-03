43f045fd9a9a2481bf9f330bd1d485d7fbfb83f5
Eduardo Pool <eduardo.alejandro.pool.ake@gmail.com>
2016-05-03 14:45:06 -0700

    Migrate stetho interceptor from Java to Kotlin.

---
+++ app/src/main/java/com/nearsoft/nearbooks/other/StethoInterceptor.kt
@@ -0,0 +1,216 @@
+package com.nearsoft.nearbooks.other
+
+import com.facebook.stetho.inspector.network.DefaultResponseHandler
+import com.facebook.stetho.inspector.network.NetworkEventReporter
+import com.facebook.stetho.inspector.network.NetworkEventReporterImpl
+import com.facebook.stetho.inspector.network.RequestBodyHelper
+import okhttp3.*
+import okio.BufferedSource
+import okio.Okio
+import java.io.IOException
+import java.io.InputStream
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * Provides easy integration with [OkHttp](http://square.github.io/okhttp/) 2.2.0+
+ * by way of the new [Interceptor](https://github.com/square/okhttp/wiki/Interceptors)
+ * system. To use:
+ *
+ * OkHttpClient client = new OkHttpClient();
+ * client.networkInterceptors().add(new StethoInterceptor());
+ *
+ */
+class StethoInterceptor : Interceptor {
+    private val mEventReporter = NetworkEventReporterImpl.get()
+
+    private val mNextRequestId = AtomicInteger(0)
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val requestId = mNextRequestId.andIncrement.toString()
+
+        val request = chain.request()
+
+        var requestBodyHelper: RequestBodyHelper? = null
+        if (mEventReporter.isEnabled) {
+            requestBodyHelper = RequestBodyHelper(mEventReporter, requestId)
+            val inspectorRequest = OkHttpInspectorRequest(requestId, request, requestBodyHelper)
+            mEventReporter.requestWillBeSent(inspectorRequest)
+        }
+
+        var response: Response
+        try {
+            response = chain.proceed(request)
+        } catch (e: IOException) {
+            if (mEventReporter.isEnabled) {
+                mEventReporter.httpExchangeFailed(requestId, e.toString())
+            }
+            throw e
+        }
+
+        if (mEventReporter.isEnabled) {
+            if (requestBodyHelper != null && requestBodyHelper.hasBody()) {
+                requestBodyHelper.reportDataSent()
+            }
+
+            val connection = chain.connection()
+            mEventReporter.responseHeadersReceived(
+                    OkHttpInspectorResponse(
+                            requestId,
+                            request,
+                            response,
+                            connection))
+
+            val body = response.body()
+            var contentType: MediaType? = null
+            var responseStream: InputStream? = null
+            if (body != null) {
+                contentType = body.contentType()
+                responseStream = body.byteStream()
+            }
+
+            responseStream = mEventReporter.interpretResponseStream(
+                    requestId,
+                    if (contentType != null) contentType.toString() else null,
+                    response.header("Content-Encoding"),
+                    responseStream,
+                    DefaultResponseHandler(mEventReporter, requestId))
+            if (responseStream != null) {
+                response = response.newBuilder().body(ForwardingResponseBody(body, responseStream)).build()
+            }
+        }
+
+        return response
+    }
+
+    private class OkHttpInspectorRequest(
+            private val mRequestId: String,
+            private val mRequest: Request,
+            private val mRequestBodyHelper: RequestBodyHelper) : NetworkEventReporter.InspectorRequest {
+
+        override fun id(): String {
+            return mRequestId
+        }
+
+        override fun friendlyName(): String? {
+            // Hmm, can we do better?  tag() perhaps?
+            return null
+        }
+
+        override fun friendlyNameExtra(): Int? {
+            return null
+        }
+
+        override fun url(): String {
+            return mRequest.url().toString()
+        }
+
+        override fun method(): String {
+            return mRequest.method()
+        }
+
+        @Throws(IOException::class)
+        override fun body(): ByteArray? {
+            val body = mRequest.body() ?: return null
+            val out = mRequestBodyHelper.createBodySink(firstHeaderValue("Content-Encoding"))
+            val bufferedSink = Okio.buffer(Okio.sink(out))
+            try {
+                body.writeTo(bufferedSink)
+            } finally {
+                bufferedSink.close()
+            }
+            return mRequestBodyHelper.displayBody
+        }
+
+        override fun headerCount(): Int {
+            return mRequest.headers().size()
+        }
+
+        override fun headerName(index: Int): String {
+            return mRequest.headers().name(index)
+        }
+
+        override fun headerValue(index: Int): String {
+            return mRequest.headers().value(index)
+        }
+
+        override fun firstHeaderValue(name: String): String? {
+            return mRequest.header(name)
+        }
+    }
+
+    private class OkHttpInspectorResponse(
+            private val mRequestId: String,
+            private val mRequest: Request,
+            private val mResponse: Response,
+            private val mConnection: Connection) : NetworkEventReporter.InspectorResponse {
+
+        override fun requestId(): String {
+            return mRequestId
+        }
+
+        override fun url(): String {
+            return mRequest.url().toString()
+        }
+
+        override fun statusCode(): Int {
+            return mResponse.code()
+        }
+
+        override fun reasonPhrase(): String {
+            return mResponse.message()
+        }
+
+        override fun connectionReused(): Boolean {
+            // Not sure...
+            return false
+        }
+
+        override fun connectionId(): Int {
+            return mConnection.hashCode()
+        }
+
+        override fun fromDiskCache(): Boolean {
+            return mResponse.cacheResponse() != null
+        }
+
+        override fun headerCount(): Int {
+            return mResponse.headers().size()
+        }
+
+        override fun headerName(index: Int): String {
+            return mResponse.headers().name(index)
+        }
+
+        override fun headerValue(index: Int): String {
+            return mResponse.headers().value(index)
+        }
+
+        override fun firstHeaderValue(name: String): String? {
+            return mResponse.header(name)
+        }
+    }
+
+    private class ForwardingResponseBody(private val mBody: ResponseBody, interceptedStream: InputStream) : ResponseBody() {
+        private val mInterceptedSource: BufferedSource
+
+        init {
+            mInterceptedSource = Okio.buffer(Okio.source(interceptedStream))
+        }
+
+        override fun contentType(): MediaType {
+            return mBody.contentType()
+        }
+
+        override fun contentLength(): Long {
+            return mBody.contentLength()
+        }
+
+        override fun source(): BufferedSource {
+            // close on the delegating body will actually close this intercepted source, but it
+            // was derived from mBody.byteStream() therefore the close will be forwarded all the
+            // way to the original.
+            return mInterceptedSource
+        }
+    }
+}
