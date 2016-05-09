bed47dabfb1d3bdbff92a142847d8200e3e1945e
Eduardo Pool <eduardo.alejandro.pool.ake@gmail.com>
2016-05-08 21:52:33 -0700

    Add multidex support again.

--- app/src/main/java/com/nearsoft/nearbooks/NearbooksApplication.kt
+++ app/src/main/java/com/nearsoft/nearbooks/NearbooksApplication.kt
@@ -1,6 +1,6 @@
 package com.nearsoft.nearbooks
 
-import android.app.Application
+import android.support.multidex.MultiDexApplication
 import com.nearsoft.nearbooks.di.components.DaggerNearbooksApplicationComponent
 import com.nearsoft.nearbooks.di.components.NearbooksApplicationComponent
 import com.nearsoft.nearbooks.di.modules.NearbooksApplicationModule
@@ -11,7 +11,7 @@ import com.squareup.picasso.Picasso
  * Base Nearbooks application.
  * Created by epool on 11/17/15.
  */
-class NearbooksApplication : Application() {
+class NearbooksApplication : MultiDexApplication() {
 
     companion object {
         private var sApplicationComponent: NearbooksApplicationComponent? = null
